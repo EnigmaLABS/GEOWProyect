cdce1c8ed7295bf817534c9e75305688401685ba
iván martín <ivan.martin.78@gmail.com>
2020-06-10 14:44:42 +0200

    end journey OK

---
+++ GEOWDB/GEOWDB/dbo/Stored Procedures/EndJourney.sql
@@ -0,0 +1,10 @@
+﻿create procedure EndJourney
+@idJourney			bigint
+as
+begin
+	update Journeys
+	set
+	dtFin = GETDATE()
+	where
+	idJourney = @idJourney
+end
--- GEOWDB/GEOWDB/dbo/Tables/Journeys.sql
+++ GEOWDB/GEOWDB/dbo/Tables/Journeys.sql
@@ -1,6 +1,9 @@
 ﻿CREATE TABLE [dbo].[Journeys] (
     [idJourney] BIGINT   IDENTITY (1, 1) NOT NULL,
     [dtInicio]  DATETIME CONSTRAINT [DF_JourneyCounter_dtInicio] DEFAULT (getdate()) NULL,
+    [dtFin]     DATETIME NULL,
     CONSTRAINT [PK_JourneyCounter] PRIMARY KEY CLUSTERED ([idJourney] ASC)
 );
+
+
 
